38f66f3232eda03dfe7b7fa63044bb6f56ac86fa
Yohannes Kifle <yohannes2kifle@gmail.com>
2021-06-19 22:41:31 +0300

    Removes unnecessary handlebar customizations

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,15 @@ extern crate dotenv_codegen;
 extern crate rocket;
 use rocket::fs::FileServer;
 use rocket::response::Redirect;
-use rocket_dyn_templates::{handlebars, Template};
-
-use self::handlebars::{Handlebars, JsonRender};
+use rocket_dyn_templates::{Template};
 
 mod database;
-mod routes;
 mod models;
+mod routes;
 
 #[get("/favicon.ico")]
 fn favicon() -> Redirect {
     Redirect::to(uri!("/static/favicon.ico"))
-}
-
-fn wow_helper(
-    h: &handlebars::Helper<'_, '_>,
-    _: &handlebars::Handlebars,
-    _: &handlebars::Context,
-    _: &mut handlebars::RenderContext<'_, '_>,
-    out: &mut dyn handlebars::Output,
-) -> handlebars::HelperResult {
-    if let Some(param) = h.param(0) {
-        out.write("<b><i>")?;
-        out.write(&param.value().render())?;
-        out.write("</b></i>")?;
-    }
-
-    Ok(())
-}
-
-fn customize(hbs: &mut Handlebars) {
-    hbs.register_helper("wow", Box::new(wow_helper));
 }
 
 #[launch]
@@ -45,7 +23,5 @@ fn rocket() -> _ {
         .mount("/", routes![favicon])
         .attach(routes::home::stage())
         .attach(routes::accounts::stage())
-        .attach(Template::custom(|engines| {
-            customize(&mut engines.handlebars);
-        }))
+        .attach(Template::custom(|_| {}))
 }
